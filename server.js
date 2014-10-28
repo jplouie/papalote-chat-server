a0a2edfebbb3b7c9002698923f12c8f6e291f1ac
Jeff Louie <jpl7690@yahoo.com>
2014-10-28 10:40:14 -0700

    edited socket name

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ io.on('connection',function(socket) {
 
   socket.on('chat message', function(msg) {
     console.log("msg: " + msg);
-    io.emit('chat message', msg);
+    io.emit('receive message', msg);
   });
 })
 
